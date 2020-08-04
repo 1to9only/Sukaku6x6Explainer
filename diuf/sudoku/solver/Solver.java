acb076a84f7118c373aa74f72216b9015dfc08c8
1to9only <53971101+1to9only@users.noreply.github.com>
2020-08-04 07:19:09 +0100

    small code changes/improvements

--- diuf/sudoku/solver/Solver.java
+++ diuf/sudoku/solver/Solver.java
@@ -347,7 +347,7 @@ public class Solver {
         return result;
     }
 
-    private boolean isSolved() {
+    public boolean isSolved() {
         for (int y = 0; y < 6; y++) {
             for (int x = 0; x < 6; x++) {
                 if (grid.getCellValue(x, y) == 0)
