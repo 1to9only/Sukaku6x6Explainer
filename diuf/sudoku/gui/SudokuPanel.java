f2152546ccc76a3ea34a02cdef413b62d6467420
1to9only <1to9only@protonmail.com>
2023-04-05 10:39:39 +0100

    change to showing candidate masks

--- diuf/sudoku/gui/SudokuPanel.java
+++ diuf/sudoku/gui/SudokuPanel.java
@@ -451,6 +451,11 @@ public class SudokuPanel extends JPanel {
                     (col.getBlue() + Color.orange.getBlue()) / 2);
         if (cell == selectedCell && value == focusedCandidate)
             col = Color.black;
+        if ( Settings.getInstance().isShowingCandidateMasks() ) {
+            if ( null != selectedCell && selectedCell.getValue() == value ) {
+                col = Color.red;
+            }
+        }
         g.setColor(col);
         return isHighlighted;
     }
@@ -506,25 +511,25 @@ public class SudokuPanel extends JPanel {
             col = Color.orange;
         else if (cell == focusedCell)
             col = Color.yellow;
-        else {
-            boolean showing = Settings.getInstance().isShowingCandidateMasks();
-            if ( showing == true ) {
-                // Selected candidates color
-                int value = -1;
-                if (null != selectedCell) {
-                    value = selectedCell.getValue();
-                }
-                if ( value > 0 ) {
-                    if ( value == cell.getValue()) {
-                        col = candidateMaskColor;
-                    }
-                    else
-                    if ( cell.hasPotentialValue(value)) {
-                        col = potentialMaskColor;
-                    }
-                }
-            }
-        }
+    /// else {
+    ///     boolean showing = Settings.getInstance().isShowingCandidateMasks();
+    ///     if ( showing == true ) {
+    ///         // Selected candidates color
+    ///         int value = -1;
+    ///         if (null != selectedCell) {
+    ///             value = selectedCell.getValue();
+    ///         }
+    ///         if ( value > 0 ) {
+    ///             if ( value == cell.getValue()) {
+    ///                 col = candidateMaskColor;
+    ///             }
+    ///             else
+    ///             if ( cell.hasPotentialValue(value)) {
+    ///                 col = potentialMaskColor;
+    ///             }
+    ///         }
+    ///     }
+    /// }
         g.setColor(col);
     }
 
@@ -538,25 +543,25 @@ public class SudokuPanel extends JPanel {
     //      col = Color.orange;
     //  else if (cell == focusedCell)
     //      col = Color.yellow;
-        else {
-            boolean showing = Settings.getInstance().isShowingCandidateMasks();
-            if ( showing == true ) {
-                // Selected candidates color
-                int value = -1;
-                if (null != selectedCell) {
-                    value = selectedCell.getValue();
-                }
-                if ( value > 0 ) {
-                    if ( value == cell.getValue()) {
-                        col = candidateMaskColor;
-                    }
-                    else
-                    if ( cell.hasPotentialValue(value)) {
-                        col = potentialMaskColor;
-                    }
-                }
-            }
-        }
+    /// else {
+    ///     boolean showing = Settings.getInstance().isShowingCandidateMasks();
+    ///     if ( showing == true ) {
+    ///         // Selected candidates color
+    ///         int value = -1;
+    ///         if (null != selectedCell) {
+    ///             value = selectedCell.getValue();
+    ///         }
+    ///         if ( value > 0 ) {
+    ///             if ( value == cell.getValue()) {
+    ///                 col = candidateMaskColor;
+    ///             }
+    ///             else
+    ///             if ( cell.hasPotentialValue(value)) {
+    ///                 col = potentialMaskColor;
+    ///             }
+    ///         }
+    ///     }
+    /// }
         g.setColor(col);
     }
 
